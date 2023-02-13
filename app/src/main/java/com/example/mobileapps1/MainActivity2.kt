91b58fd1fe93b21410b6038e19e7ca5d7f4136a5
Daphyne <daphyniedamaia@gmail.com>
2023-02-13 15:25:54 +0000

    Understand activity stack Default behaviour when navigating to a new activity onBackPressed

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -3,26 +3,30 @@ package com.example.mobileapps1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        Log.d("main_activity", "Activity created")
+
+        val nextPageButton = findViewById<Button>(R.id.nextPageButton)
+        nextPageButton.setOnClickListener{
+
+//             this creates a new activity
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+
+//            do not use onDestroy to end an activity
+        // onDestroy()
+            finish()
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.w("main_activity", "Activity has been paused")
+    @Deprecated("For learning purposes only")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.e("m2", "Back button pressed")
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.e("main_activity", "This activity has been resumed")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e("main_activity", "Activity has been destroyed")
-    }
 }
